694450ddaad9f66042edaca00a7f8f6b4d6d1cfb
Cody Gula <codygula@yahoo.com>
2022-07-03 16:36:45 -0700

    improvements to ui

--- code.py
+++ code.py
@@ -24,16 +24,38 @@ def download(s3Object, destFile):
 def imageResize(picture, thumbNailName):
     image1 = Image.open(picture)
     width, height = image1.size
-    newSize = (300, 300)
+    newSize = (200, 200)
     image2 = image1.resize(newSize)
     image2.save(thumbNailName)
 
-i=1
-while i <= 4:
+
+# Get names of objects in bucket
+s3Bucket = s3.Bucket("newtestbucket25324dhfghgfhd8gds0")
+
+listOfPhotos = []
+
+for thing in s3Bucket.objects.all():
+    print(thing.key)
+    print(type(thing))
+    # if str(thing).endswith(".jpg" or ".png" or ".raw"):
+    # print("Success!")
+    listOfPhotos.append(thing.key)
+
+print(listOfPhotos)
+
+
+# Iterate over names 
+i=0
+while i <= (len(listOfPhotos) - 1):
     destFile1 = "/tmp/testingtestingtesting" + str(i) + ".jpg"
-    s3Object1 = "Picture" + str(i) + ".jpg"
-    download(s3Object1, destFile1)
-    imageResize(destFile1, "thumbnailimg" + str(i) + ".jpg")
+    s3Object1 = listOfPhotos[i]
+    try:
+        download(s3Object1, destFile1)
+        imageResize(destFile1, "thumbnailimg" + str(i) + ".jpg")
+    except:
+        print("error")
+        destFile1 = "img049.jpg"
+        imageResize(destFile1, "thumbnailimg" + str(i) + ".jpg")
     i = i + 1
 
 
@@ -90,6 +112,86 @@ class Ui(QDialog):
         labelImage.setPixmap(pixmap)
         vbox.addWidget(labelImage)
         self.Photo6.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg7.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo7.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg8.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo8.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg9.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo9.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg10.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo10.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg11.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo11.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg12.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo12.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg13.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo13.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg14.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo14.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg15.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo15.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg5.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo5.setLayout(vbox)
         self.show()
 
 
