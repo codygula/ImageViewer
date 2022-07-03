0f2a1ced46fcf68209ef189ab0264f91f047b31e
Cody Gula <codygula@yahoo.com>
2022-07-03 11:33:17 -0700

    it now works, kind of

--- code.py
+++ code.py
@@ -1,5 +1,4 @@
-# TODO fix class. This should be fairly easy. get the class to work, call it for each open qlabel in the interface.ui
-#
+# This is very inelegant, but it mostly works
 
 
 from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel 
@@ -11,38 +10,31 @@ from PIL import Image
 import boto3
 import uuid
 
-# Class to display a thumbnail image
-class Photothumbnail:
-    def __init__(self, prefix, suffix):
-        #self.bucketName = bucketName
-        #self.objectName = objectName
-        #self.fileName = fileName
-        self.prefix = prefix
-        self.suffix = suffix
-
-    def getObjectName(self):
-        return self.prefix + self.suffix + ".jpg"
-
-    def getFileName(self):
-        return "/tmp/" + "s3image" + str(uuid.uuid1())
-
-    def downLoad(self):
-        s3.meta.client.download_file("newtestbucket25324dhfghgfhd8gds0", self.getObjectName, self.getFileName)
-
-
-
 s3 = boto3.resource("s3")
 
+s3Bucket = "newtestbucket25324dhfghgfhd8gds0"
 
-# image34 = "/tmp/" + "s3image" + str(uuid.uuid1())
-# s3.meta.client.download_file("newtestbucket25324dhfghgfhd8gds0", "Picture1.jpg", image34)
+# Download object from S3 and save it to a file
+def download(s3Object, destFile):
+    s3.meta.client.download_file("newtestbucket25324dhfghgfhd8gds0", s3Object, destFile)
+    print(f"downloaded {s3Object} to {destFile}")
 
-image1 = Image.open(Photothumbnail.downLoad("Picture", "1"))
 
-width, height = image1.size
-newSize = (300, 300)
-image2 = image1.resize(newSize)
-image2.save("thumbnailimg.jpg")
+# Image resize
+def imageResize(picture, thumbNailName):
+    image1 = Image.open(picture)
+    width, height = image1.size
+    newSize = (300, 300)
+    image2 = image1.resize(newSize)
+    image2.save(thumbNailName)
+
+i=1
+while i <= 4:
+    destFile1 = "/tmp/testingtestingtesting" + str(i) + ".jpg"
+    s3Object1 = "Picture" + str(i) + ".jpg"
+    download(s3Object1, destFile1)
+    imageResize(destFile1, "thumbnailimg" + str(i) + ".jpg")
+    i = i + 1
 
 
 class Ui(QDialog):
@@ -54,15 +46,55 @@ class Ui(QDialog):
 
         vbox = QVBoxLayout()
         labelImage = QLabel(self)
-        pixmap = QPixmap("thumbnailimg.jpg")
+        pixmap = QPixmap("thumbnailimg1.jpg")
         labelImage.setPixmap(pixmap)
         vbox.addWidget(labelImage)
         self.Photo1.setLayout(vbox)
         self.show()
-        # self.Photo1.picture()
-        #self.Photo2.setText("testing")
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg2.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo2.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg3.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo3.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg4.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo4.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg5.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo5.setLayout(vbox)
+        self.show()
+
+        vbox = QVBoxLayout()
+        labelImage = QLabel(self)
+        pixmap = QPixmap("thumbnailimg6.jpg")
+        labelImage.setPixmap(pixmap)
+        vbox.addWidget(labelImage)
+        self.Photo6.setLayout(vbox)
+        self.show()
 
 
 app = QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
 window = Ui() # Create an instance of our class
 app.exec_() # Start the application
+
+
