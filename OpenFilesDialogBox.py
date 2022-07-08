55dc32875fab1cad3938a362417ee7bd49f11274
Cody Gula <codygula@yahoo.com>
2022-07-08 06:40:02 -0700

    Dialog box to import images

--- OpenFilesDialogBox.py
+++ OpenFilesDialogBox.py
@@ -5,12 +5,25 @@ import sys
  
 # mostly stolen from https://coderslegacy.com/python/pyqt5-file-dialog/
 
+
+
 def dialog():
+    
     file , check = QFileDialog.getOpenFileNames(None, "QFileDialog.getOpenFileName()",
                                                "", "All Files (*);;Python Files (*.py);;Text Files (*.txt)")
     if check:
-        print(file)
- 
+        print("Selected Files: " + str(file))
+        files = []
+        print(type(file))
+        print(type(files))
+        
+        for i in file:
+            files.append(i)
+        
+
+        print("The selected files are ", files)
+        return files
+
 app = QApplication(sys.argv)
 win = QMainWindow()
 win.setGeometry(400,400,300,300)
